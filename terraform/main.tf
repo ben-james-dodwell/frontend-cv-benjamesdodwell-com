1d7c471ab3f9d09683a9cb3322d83ab80d35ca14
Ben James Dodwell <61091068+ben-james-dodwell@users.noreply.github.com>
2024-05-08 10:08:04 +0100

    Initial commit

---
+++ terraform/main.tf
@@ -0,0 +1,11 @@
+# Create S3 bucket
+
+# Upload file to S3 bucket
+
+# Create CloudFront distribution
+
+# Invalidate CloudFront cache if S3 bucket updated
+
+# Create ACM certificate
+
+# Create DNS records
